841324c1e814658a14e7e75d8b15bc5309d8e0f7
esamani77 <esamani77@gmail.com>
2023-02-19 16:27:30 +0330

    fix(comment): update handleLikeComment fun  | relate #10

--- src/components/Discussion/components/Comment.tsx
+++ src/components/Discussion/components/Comment.tsx
@@ -10,134 +10,110 @@ import CommentInput from "./CommentInput";
 import CommentText from "./CommentText";
 
 interface ICommentsProps extends IComment {
-	replies?: IComment[];
+  replies?: IComment[];
 }
 
 export const Comment = (props: ICommentsProps) => {
-	const hasReplies = props?.replies;
-	const [user] = useUser();
-	const queryClient = useQueryClient();
+  const hasReplies = props?.replies;
+  const [user] = useUser();
+  const queryClient = useQueryClient();
+  const [addReply, setAddReply] = useState(false);
+  const handleLikeComment = (date: number, id: number) => {
+    queryClient.setQueryData(
+      [Disscusion],
+      // ✅ this is the way
+      (oldData: IDiscussion[] | undefined) => {
+        if (oldData) {
+          const clonedDiscussion = structuredClone(oldData) as IDiscussion[];
+          const updatedDiscussion = clonedDiscussion.map((item) => {
+            if (item.date === date && item.id === id && !item.iLikedIt) {
+              return {
+                ...item,
+                iLikedIt: true,
+                likes: item.likes + 1,
+              };
+            }
+            if (item.replies && item.replies.length > 0) {
+              item.replies = item.replies.map((reply) => {
+                if (reply.date === date && reply.id === id && !reply.iLikedIt) {
+                  return {
+                    ...reply,
+                    iLikedIt: true,
+                    likes: reply.likes + 1,
+                  };
+                }
+                return reply;
+              });
+            }
+            return item;
+          });
 
-	const handleAddCommnet = ({ commentId, reply }: { commentId: number; reply: IComment }) => {
-		queryClient.setQueryData(
-			[Disscusion],
-			// ✅ this is the way
-			(oldData: IDiscussion[] | undefined) => {
-				if (oldData) {
-					const updatedDiscussion = structuredClone(oldData) as IDiscussion[];
-					updatedDiscussion.map((item) => {
-						if (item.id === commentId) {
-							item.replies = [
-								...item.replies,
-								{
-									iLikedIt: false,
-									likes: 0,
-									text: reply.text,
-									user: reply.user,
-									date: reply.date,
-									id: Number((Math.random() * 1000).toFixed()),
-								},
-							];
-						}
-						return item;
-					});
+          return updatedDiscussion;
+        }
+      }
+    );
+  };
 
-					return updatedDiscussion;
-				}
-			},
-		);
-	};
-	const handleLikeComment = (date: number, id: number) => {
-		queryClient.setQueryData(
-			[Disscusion],
-			// ✅ this is the way
-			(oldData: IDiscussion[] | undefined) => {
-				if (oldData) {
-					const clonedDiscussion = structuredClone(oldData) as IDiscussion[];
-					const updatedDiscussion = clonedDiscussion.map((item) => {
-						if (item.date === date && item.id === id && !item.iLikedIt) {
-							return {
-								...item,
-								iLikedIt: true,
-								likes: item.likes + 1,
-							};
-						}
-						if (item.replies && item.replies.length > 0) {
-							item.replies = item.replies.map((reply) => {
-								if (reply.date === date && reply.id === id && !reply.iLikedIt) {
-									return {
-										...reply,
-										iLikedIt: true,
-										likes: reply.likes + 1,
-									};
-								}
-								return reply;
-							});
-						}
-						return item;
-					});
-
-					return updatedDiscussion;
-				}
-			},
-		);
-	};
-	return (
-		<div className="flex gap-2  my-xs" key={props.id}>
-			<Avatar
-				alt={props.user.name}
-				src={props.user?.avatar}
-				width={48}
-				height={48}
-				className="rounded-full"
-			/>
-			<div className="flex gap-2 flex-col">
-				<div className="flex items-center gap-2">
-					<p className="text-base text-bold">{props.user.name}</p>
-					<small className="text-xs">{timeAgo(props.date)}</small>
-				</div>
-				<CommentText text={props.text} />
-				<div className="flex gap-2 items-center">
-					<p
-						className={`items-center flex gap-2 rounded-xs py-xss px-xs ${
-							props.iLikedIt ? "bg-primary" : "bg-light-gray"
-						}`}
-						onClick={() => handleLikeComment(props.date, props.id)}
-					>
-						<Like1 size={24} color={props.iLikedIt ? "#FFF" : "gray"} variant="Bold" />
-						<span className={`${props.iLikedIt ? "text-white" : "text-black"}`}>{props.likes}</span>
-					</p>
-					{props.replies && (
-						<p
-							onClick={() =>
-								handleAddCommnet({
-									commentId: props.id,
-									reply: {
-										date: Date.now(),
-										id: 1,
-										iLikedIt: false,
-										likes: 0,
-										text: "this is a test comment",
-										user: user,
-									},
-								})
-							}
-						>
-							reply
-						</p>
-					)}
-				</div>
-				{hasReplies && hasReplies.length > 0 && (
-					<div className="border-l">
-						{hasReplies.map((reply) => {
-							return <Comment {...reply} key={reply.id} />;
-						})}
-						<CommentInput />
-					</div>
-				)}
-			</div>
-		</div>
-	);
+  const ref = useRef<HTMLTextAreaElement>(null);
+  return (
+    <div className="flex gap-2  my-xs">
+      <Avatar
+        alt={props.user.name}
+        src={props.user?.avatar}
+        width={48}
+        height={48}
+        className="rounded-full"
+      />
+      <div className="flex gap-2 flex-col">
+        <div className="flex items-center gap-2">
+          <p className="text-base text-bold">{props.user.name}</p>
+          <small className="text-xs">{timeAgo(props.date)}</small>
+        </div>
+        <CommentText text={props.text} />
+        <div className="flex gap-2 items-center">
+          <p
+            className={`items-center flex gap-2 rounded-xs py-xss px-xs ${
+              props.iLikedIt ? "bg-primary" : "bg-light-gray"
+            }`}
+            onClick={() => handleLikeComment(props.date, props.id)}
+          >
+            <Like1
+              size={24}
+              color={props.iLikedIt ? "#FFF" : "gray"}
+              variant="Bold"
+            />
+            <span className={`${props.iLikedIt ? "text-white" : "text-black"}`}>
+              {props.likes}
+            </span>
+          </p>
+          {props.replies && (
+            <p
+              onClick={async () => {
+                if (hasReplies && hasReplies.length === 0) {
+                  setAddReply(true);
+                  await sleep(200);
+                }
+                ref.current && ref.current.focus();
+              }}
+            >
+              reply
+            </p>
+          )}
+        </div>
+        {hasReplies && hasReplies.length > 0 && (
+          <div className="border-l">
+            {hasReplies.map((reply) => {
+              return <Comment {...reply} key={reply.id} />;
+            })}
+            <CommentInput ref={ref} commentId={props.id} />
+          </div>
+        )}
+        {hasReplies && hasReplies.length === 0 && addReply && (
+          <CommentInput ref={ref} commentId={props.id} />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Comment;
